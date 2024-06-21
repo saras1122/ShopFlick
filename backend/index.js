368886fe1aeb723111bea5f303e755327a785549
saras1122 <sarasnagaria1@gmail.com>
2024-06-22 00:26:57 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,6 @@ const app = express()
 app.use(cors({
     origin: process.env.FRONTEND_URL,
     credentials: true,
-    methods: ['GET','PUT' ,'POST', 'DELETE'],
 }));
 app.use(express.json())
 app.use(cookieParser())
