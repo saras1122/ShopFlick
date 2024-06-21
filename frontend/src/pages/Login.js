26a3340fa7d0fd108c4c1d2b00ba8e2ba1368f89
saras1122 <sarasnagaria1@gmail.com>
2024-06-21 19:26:42 +0530

    first commit

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -8,95 +8,98 @@ import { toast } from 'react-toastify';
 import Context from '../context';
 
 const Login = () => {
-    const [showPassword, setShowPassword] = useState(false)
-    const [data, setData] = useState({
-        email: "",
-        password: ""
+    const [showPassword,setShowPassword] = useState(false)
+    const [data,setData] = useState({
+        email : "",
+        password : ""
     })
     const navigate = useNavigate()
-    const {fetchUserDetails}=useContext(Context)
-    const handleOnChange = (e) => {
-        const { name, value } = e.target
+    const { fetchUserDetails, fetchUserAddToCart } = useContext(Context)
 
-        setData((preve) => {
-            return {
+    const handleOnChange = (e) =>{
+        const { name , value } = e.target
+
+        setData((preve)=>{
+            return{
                 ...preve,
-                [name]: value
+                [name] : value
             }
         })
     }
 
 
-    const handleSubmit = async (e) => {
+    const handleSubmit = async(e) =>{
         e.preventDefault()
 
-        const dataResponse = await fetch(SummaryApi.signIn.url, {
-            method: SummaryApi.signIn.method,
-            credentials:'include',
-            headers: {
-                "content-type": "application/json"
+        const dataResponse = await fetch(SummaryApi.signIn.url,{
+            method : SummaryApi.signIn.method,
+            credentials : 'include',
+            headers : {
+                "content-type" : "application/json"
             },
-            body: JSON.stringify(data)
+            body : JSON.stringify(data)
         })
+
         const dataApi = await dataResponse.json()
 
-        if (dataApi.success) {
+        if(dataApi.success){
             toast.success(dataApi.message)
-            fetchUserDetails()
             navigate('/')
+            fetchUserDetails()
+            fetchUserAddToCart()
         }
 
-        if (dataApi.error) {
+        if(dataApi.error){
             toast.error(dataApi.message)
         }
 
     }
 
-    console.log("data login", data)
+    console.log("data login",data)
+    
+  return (
+    <section id='login'>
+        <div className='mx-auto container p-4'>
 
-    return (
-        <section id='login'>
-            <div className='mx-auto container p-4'>
-
-                <div className='bg-white p-5 w-full max-w-sm mx-auto'>
+            <div className='bg-white p-5 w-full max-w-sm mx-auto'>
                     <div className='w-20 h-20 mx-auto'>
-                        <img src={loginIcons} alt='login icons' />
+                        <img src={loginIcons} alt='login icons'/>
                     </div>
 
                     <form className='pt-6 flex flex-col gap-2' onSubmit={handleSubmit}>
                         <div className='grid'>
                             <label>Email : </label>
                             <div className='bg-slate-100 p-2'>
-                                <input
-                                    type='email'
-                                    placeholder='enter email'
+                                <input 
+                                    type='email' 
+                                    placeholder='enter email' 
                                     name='email'
                                     value={data.email}
                                     onChange={handleOnChange}
-                                    className='w-full h-full outline-none bg-transparent' />
+                                    className='w-full h-full outline-none bg-transparent'/>
                             </div>
                         </div>
 
                         <div>
                             <label>Password : </label>
                             <div className='bg-slate-100 p-2 flex'>
-                                <input
-                                    type={showPassword ? "text" : "password"}
+                                <input 
+                                    type={showPassword ? "text" : "password"} 
                                     placeholder='enter password'
                                     value={data.password}
-                                    name='password'
+                                    name='password' 
                                     onChange={handleOnChange}
-                                    className='w-full h-full outline-none bg-transparent' />
-                                <div className='cursor-pointer text-xl' onClick={() => setShowPassword((preve) => !preve)}>
+                                    className='w-full h-full outline-none bg-transparent'/>
+                                <div className='cursor-pointer text-xl' onClick={()=>setShowPassword((preve)=>!preve)}>
                                     <span>
                                         {
                                             showPassword ? (
-                                                <FaEyeSlash />
+                                                <FaEyeSlash/>
                                             )
-                                                :
-                                                (
-                                                    <FaEye />
-                                                )
+                                            :
+                                            (
+                                                <FaEye/>
+                                            )
                                         }
                                     </span>
                                 </div>
@@ -111,12 +114,12 @@ const Login = () => {
                     </form>
 
                     <p className='my-5'>Don't have account ? <Link to={"/sign-up"} className=' text-red-600 hover:text-red-700 hover:underline'>Sign up</Link></p>
-                </div>
-
-
             </div>
-        </section>
-    )
+
+
+        </div>
+    </section>
+  )
 }
 
 export default Login
