7d595f613c0ead17361f60ea6a12ec2deb8ab3aa
saras1122 <sarasnagaria1@gmail.com>
2024-06-21 19:35:16 +0530

    first commit

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -14,7 +14,7 @@ const Login = () => {
         password : ""
     })
     const navigate = useNavigate()
-    const { fetchUserDetails, fetchUserAddToCart } = useContext(Context)
+    const { fetchUserDetails } = useContext(Context)
 
     const handleOnChange = (e) =>{
         const { name , value } = e.target
@@ -46,7 +46,7 @@ const Login = () => {
             toast.success(dataApi.message)
             navigate('/')
             fetchUserDetails()
-            fetchUserAddToCart()
+            
         }
 
         if(dataApi.error){
